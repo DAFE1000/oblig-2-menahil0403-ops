c4b90d62724a48d574b39ce719e905c3fb018adc
Menahil P <menahil0403@gmail.com>
2026-03-08 16:53:41 +0000

    løsning på oppgaven

--- løsningen/oblig2.py
+++ løsningen/oblig2.py
@@ -1,9 +1,28 @@
 import math
 
-#funksjonen 
+#funksjonen f
 def f(x):
     return math.exp(-x/4) * math.atan(x)
 
-#likningen som skal løses
+#funksjonen g
 def g(x):
     return math.atan(x) - 4/(x**2 + 1)
+
+#definerer start punkter(basert på det jeg vant ved analytiske løsning)
+a= 0
+b= 4
+
+#lager en løkka som finner løsning på oppgaven
+for i in range(100):
+    m = (a + b)/2
+
+    if g(a)*g(m) < 0:
+        b = m
+    else:
+        a = m
+
+x = (a + b) / 2
+y = f(x)
+
+print("x = ", round(x,4))
+print("y = ", round(y,4))
